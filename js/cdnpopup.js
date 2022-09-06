ae6e0f4eef17cfe3db40b1b192bd93d45a012348
Eriklebson Martins De Paiva <eriklebson2016@gmail.com>
2022-09-06 13:44:05 -0300

    v2

--- js/cdnpopup.js
+++ js/cdnpopup.js
@@ -1,3 +1,3 @@
 $(function(){
-    $("#includedContent").load("https://cdn.jsdelivr.net/gh/Eriklebson/cdn-popup@564d291a58426cb89d1928c10f4d5490867ca62d/popup.html");
+    $("#includedContent").load("https://cdn.jsdelivr.net/gh/Eriklebson/cdn-popup@3b7ecf7731ae94b30895e5100f81e335c90bb9f9/popup.html");
 })
