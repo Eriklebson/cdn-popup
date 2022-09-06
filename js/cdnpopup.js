d6ee3dba4c983043305cf041e83e03b693606c58
Eriklebson Martins De Paiva <eriklebson2016@gmail.com>
2022-09-06 12:19:39 -0300

    terceiro commit

--- js/cdnpopup.js
+++ js/cdnpopup.js
@@ -1,3 +1,3 @@
 $(function(){
-    $("#includedContent").load("popup.html");
+    $("#includedContent").load("https://cdn.jsdelivr.net/gh/Eriklebson/cdn-popup@3716f75043f6c06af14de5eb0fa7f9f8c31a27dc/popup.html");
 })
