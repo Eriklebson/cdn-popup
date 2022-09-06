be7bc9cc0a0fe1c3b3788cad18f948496c9fe6bc
Eriklebson Martins De Paiva <eriklebson2016@gmail.com>
2022-09-06 12:27:21 -0300

    correção de versãogit

--- js/cdnpopup.js
+++ js/cdnpopup.js
@@ -1,3 +1,3 @@
 $(function(){
-    $("#includedContent").load("https://cdn.jsdelivr.net/gh/Eriklebson/cdn-popup@3716f75043f6c06af14de5eb0fa7f9f8c31a27dc/popup.html");
+    $("#includedContent").load("https://cdn.jsdelivr.net/gh/Eriklebson/cdn-popup@6db2f50004efd16ef147d448d711fd49e256acfc/popup.html");
 })
