de12067d121512011ecae398ec754460f8fc9ec2
Eriklebson Martins De Paiva <eriklebson2016@gmail.com>
2022-09-06 14:41:38 -0300

    correcao de direcionamento

--- js/cdnpopup.js
+++ js/cdnpopup.js
@@ -1,3 +1,3 @@
 $(function(){
-    $("#includedContent").load("https://cdn.jsdelivr.net/gh/Eriklebson/cdn-popup@ff49102ffc4c9b53dcd3c5da1436e942a13629f5/popup.html");
+    $("#includedContent").load("https://cdn.jsdelivr.net/gh/Eriklebson/cdn-popup@latest/popup.html");
 })
